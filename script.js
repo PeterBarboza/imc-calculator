d7f0dac546735bbf7794e4601c886db94a2e2e3d
PeterBarboza <pedrom.barboza01@gmail.com>
2021-12-13 02:00:32 -0300

    automatic decimal point function created; IMCCalc function created; clean inputs and result visor function created.

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
-let resultVisor = document.getElementById('imc-calc-result')
+const resultVisor = document.getElementById('imc-calc-result')
+
+const heightInput = document.getElementById('height')
+const weightInput = document.getElementById('weight')
+
 const cleanInputsBtn = document.getElementById('clean-inputs-btn')
 const calculateBtn = document.getElementById('calculate-btn')
 
+heightInput.addEventListener('input', editHeightInput)
+weightInput.addEventListener('input', editWeightInput)
 cleanInputsBtn.addEventListener('click', cleanInputs)
 calculateBtn.addEventListener('click', calculateIMC)
 
-function calculateIMC() {
-  const height = Number(document.getElementById('height'))
-  const weight = Number(document.getElementById('weight'))
+function editHeightInput() {
+  let height = heightInput.value
+  let hasDot = height.includes('.')
 
-  if (height.lenght == 0 || weight.lenght == 0) {
-    console.log(height.lenght)
-    console.log(weight.lenght)
+  if (height.length > 1) {
+    const firstPart = height.slice(0, 1)
+    const endPart = height.slice(1, height.length)
 
-    return
-  } else {
-    console.log(height.lenght)
-    console.log(weight.lenght)
+    if (!hasDot) {
+      let finalString = firstPart.concat('.').concat(endPart)
 
-    const squaredHeight = height.value * height.value
-    const IMCResult = weight.value / squaredHeight
-    const formatedIMCResult = IMCResult.toFixed(2)
-
-
-    resultVisor.innerText = formatedIMCResult
+      heightInput.value = finalString
+    }
   }
+}
 
+function editWeightInput() {
+  let weight = weightInput.value
+  let hasDot = weight.includes('.')
 
+  if (weight.length > 3) {
+    const firstPart = weight.slice(0, 3)
+    const endPart = weight.slice(3, weight.length)
+
+    if (!hasDot) {
+      let finalString = firstPart.concat('.').concat(endPart)
+
+      weightInput.value = finalString
+    }
+  }
+}
+
+function calculateIMC() {
+  const heightToNumber = Number(heightInput.value)
+  const weightToNumber = Number(weightInput.value)
+
+  const squaredHeight = heightToNumber * heightToNumber
+  const IMCResult = weightToNumber / squaredHeight
+  const formatedIMCResult = IMCResult.toFixed(2)
+
+  resultVisor.innerText = formatedIMCResult
 }
 
 function cleanInputs() {
-  height.value = ''
-  weight.value = ''
+  heightInput.value = ''
+  weightInput.value = ''
   resultVisor.innerText = '...'
 }
